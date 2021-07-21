72789b50c7e7f8938793af9e4eba2f9308456483
rina-tenitska <39187065+rina-tenitska@users.noreply.github.com>
2021-07-21 17:53:13 +0300

    Update IssueRestTest.cs

--- tests/IssueRestTest.cs
+++ tests/IssueRestTest.cs
@@ -3,37 +3,5 @@ using NUnit.Framework;
 
 namespace allure_nunit.tests
 {
-    [AllureOwner("baev")]
-    [AllureFeature("Issues")]
-    [AllureLabel("layer", "rest")]
-    public class IssueRestTest : BaseTest
-    {
-        private const string Owner = "allure-framework";
-        private const string Repo = "allure2";
-        private const string Title = "First Note";
-        
-        [Test]
-        [AllureTag("api", "smoke")]
-        [AllureLabel("jira", "AE-1")]
-        [AllureStory("Create new issue")]
-        [AllureLabel("microservice", "Billing")]
-        public void ShouldCreateUserNote()
-        {
-            name("Create issue via api");
-            RestSteps.CreateIssueWithTitle(Owner, Repo, Title);
-            RestSteps.ShouldSeeIssueWithTitle(Owner, Repo, Title);
-        }
-        
-        [Test]
-        [AllureTag("api", "regress")]
-        [AllureLabel("jira", "AE-1")]
-        [AllureStory("Close existing issue")]
-        [AllureLabel("microservice", "Repository")]
-        public void ShouldDeleteUserNote()
-        {
-            name("Close existing issue");
-            RestSteps.CreateIssueWithTitle(Owner, Repo, Title);
-            RestSteps.CloseIssueWithTitle(Owner, Repo, Title);
-        }
-    }
+    Assert.Equals(1, 1);
 }
